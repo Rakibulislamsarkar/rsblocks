55940557b573d302f1dae828fb21d81038610cca
Rakibul Islam Sarkar <rakibulislam1019@gmail.com>
2025-01-14 05:51:36 +0530

    themes added

--- src/app/themes/page.tsx
+++ src/app/themes/page.tsx
@@ -1,11 +1,8 @@
 import { CardsDemo } from "@/components/cards"
-import { ThemeWrapper } from "@/components/theme-wrapper"
 
 
 export default function ThemesPage() {
   return (
-    <ThemeWrapper>
       <CardsDemo />
-    </ThemeWrapper>
   )
 }
